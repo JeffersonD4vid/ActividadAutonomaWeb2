322b824f6165103f4472aec4622cddf86e927c3b
JeffersonD4vid <Jeffersonmd2002@gmail.com>
2024-09-04 22:43:44 -0500

    primir commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';    
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { NzFlexModule } from 'ng-zorro-antd/flex';
+import { UsersService } from './services/users/users.service';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
+import { RegistersService } from './services/registers/registers.service';
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, NzAvatarModule, NzFlexModule,  RouterLink, 
+  RouterOutlet, NzToolTipModule, NzDropDownModule ,NzIconModule, NzLayoutModule, NzMenuModule, NzBreadCrumbModule
+],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  isCollapsed = false;
+  title = 'app-zorro-angular';
+
+  constructor(private usersService: UsersService, public RegistersService: RegistersService){}
+
+
+  isLogged(): boolean {
+    return this.usersService.getCurrentUser() != null;
+  } 
+
+  logout(){
+    this.usersService.logout();
+  }
+}
---
+++ src/app/app.config.ts
@@ -0,0 +1,23 @@
+import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { icons } from './icons-provider';
+import { provideNzIcons } from 'ng-zorro-antd/icon';
+import { en_US, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+
+registerLocaleData(en);
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideNzIcons(icons), provideNzI18n(en_US), importProvidersFrom(FormsModule), provideAnimationsAsync(), provideHttpClient(), provideFirebaseApp(() => initializeApp({"projectId":"app-web2-12a80","appId":"1:283447622164:web:6fbada679e0931276c4501","storageBucket":"app-web2-12a80.appspot.com","apiKey":"AIzaSyARUBXEkhLWl5ycxvUHzDas0RJkr5CB4qo","authDomain":"app-web2-12a80.firebaseapp.com","messagingSenderId":"283447622164","measurementId":"G-KZ8JP092HF"})), provideAuth(() => getAuth()), provideAnalytics(() => getAnalytics()), ScreenTrackingService, UserTrackingService, provideFirestore(() => getFirestore()), provideStorage(() => getStorage()), provideFirebaseApp(() => initializeApp({"projectId":"app-pokemon-40935","appId":"1:601447069220:web:5b325caf57112e2f31cab1","storageBucket":"app-pokemon-40935.appspot.com","apiKey":"AIzaSyBrW6E7eIWh-8KeoKSU_3DKkUKi5lNHRUU","authDomain":"app-pokemon-40935.firebaseapp.com","messagingSenderId":"601447069220","measurementId":"G-25SL155R1G"})), provideAuth(() => getAuth()), provideAnalytics(() => getAnalytics()), ScreenTrackingService, UserTrackingService, provideFirestore(() => getFirestore()), provideStorage(() => getStorage())]
+};
